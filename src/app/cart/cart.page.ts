bc20f8bf2fed39f66f2dcfffbd0232dc79b3d287
hossamSharif <you@example.com>
2022-07-29 22:11:37 -0700

    befor start

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { ActionSheetController, LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { OrderDetailsPage } from '../order-details/order-details.page';
 
 @Component({
   selector: 'app-cart',
@@ -8,12 +10,67 @@ import { Route, Router } from '@angular/router';
 })
 export class CartPage implements OnInit {
 
-  constructor(private rout : Router) { }
+  constructor(private rout : Router,private loadingController:LoadingController , private toast:ToastController,private actionSheetCtl:ActionSheetController ,private modalController:ModalController) { }
 
   ngOnInit() {
   }
-  mazdDetails(){
+
+  async presentModal(id?, status?) { 
+    
+    const modal = await this.modalController.create({
+      component: OrderDetailsPage ,
+      componentProps: {
+        "item":"",
+        "status": ""
+      }
+    });
+    
+    modal.onDidDismiss().then((dataReturned) => {
+      if (dataReturned !== null) {
+        console.log(dataReturned )
+        this.doAfterDissmiss(dataReturned)
+      }
+    });
  
-    this.rout.navigate(['order-details']); 
+    return await modal.present(); 
   }
+
+   doAfterDissmiss(dataReturned){
+    this.presentToast("تم سداد المفاتورة  بنجاح , يمكنك متابعة طلبك " ,'success')
+    // this.rout.navigate(['cart']);  
+   }
+
+   async presentLoadingWithOptions(msg?) {
+    const loading = await this.loadingController.create({
+      spinner: 'bubbles',
+      mode:'ios',
+      duration: 5000,
+      message: msg,
+      translucent: true,
+     // cssClass: 'custom-class custom-loading',
+      backdropDismiss: false
+    });
+    await loading.present();
+  
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed with role:', role);
+  }
+  
+
+   async presentToast(msg,color?) {
+    const toast = await this.toast.create({
+      message: msg,
+      duration: 2000,
+      color:color,
+      cssClass:'cust_Toast',
+      mode:'ios',
+      position:'top' 
+    });
+    toast.present();
+  }
+
+   subiscribe(){
+    this.presentModal()
+   }
+
 }
--- src/app/order-details/order-details.page.ts
+++ src/app/order-details/order-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController, LoadingController, ModalController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-order-details',
@@ -7,9 +10,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailsPage implements OnInit {
 
-  constructor() { }
+  constructor(private rout : Router,private loadingController:LoadingController , private toast:ToastController,private actionSheetCtl:ActionSheetController ,private modalController:ModalController) { }
 
   ngOnInit() {
+  }
+
+  mazdDetails(){
+    this.rout.navigate(['order-details']); 
+  }
+
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtl.create({
+      header: 'سداد مبلغ ',
+      subHeader: ' ر.س 30000',
+      cssClass: 'my-custom-class',
+      mode:'ios',
+      buttons: [{
+        text: 'المحفظة',
+        // role: 'destructive',
+        icon: 'wallet',
+        // data: {
+        //   type: 'delete'
+        // },
+        
+        handler: () => {
+          this.subescribe( );
+        }
+      }, {
+        text: 'قوقل باي',
+        icon: 'logo-google',
+        data: 10,
+        handler: () => {
+          this.subescribe();
+
+        }
+      }, {
+        text: 'ابل باي',
+        icon: 'logo-apple',
+        data: 'Data value',
+        handler: () => {
+          this.subescribe();
+
+        }
+      }  , {
+        text: 'إلغاء',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
+
+ 
+
+subescribe(page?){
+   this.presentLoadingWithOptions("جاري معالجة طلبك ..")
+   setTimeout(() => {
+   
+    this.loadingController.dismiss()
+    this.closeModal()
+   }, 5000);
+}
+
+async closeModal() { 
+  await this.modalController.dismiss('data');
+}
+
+async presentLoadingWithOptions(msg?) {
+  const loading = await this.loadingController.create({
+    spinner: 'bubbles',
+    mode:'ios',
+    duration: 5000,
+    message: msg,
+    translucent: true,
+   // cssClass: 'custom-class custom-loading',
+    backdropDismiss: false
+  });
+  await loading.present();
+
+  const { role, data } = await loading.onDidDismiss();
+  console.log('Loading dismissed with role:', role);
+}
+
+  async presentToast(msg,color?) {
+    const toast = await this.toast.create({
+      message: msg,
+      duration: 2000,
+      color:color,
+      cssClass:'cust_Toast',
+      mode:'ios',
+      position:'top' 
+    });
+    toast.present();
   }
 
 }
